3d4cabdd10804025be83b0ec6295e2d320d48875
TrixieI <fima1fim@hotmail.com>
2021-12-23 12:12:33 +0200

    update

---
+++ script.js
@@ -0,0 +1,27 @@
+let card = document.getElementsByClassName('card');
+let backCard = document.getElementsByClassName('backCard');
+let frontCard = document.getElementsByClassName('frontCard');
+let numberOfGuesses = 0;
+
+
+backCard.addEventListener('click', flip)
+
+function flip(e){
+
+	let firstFlip = e.target.style.(transform flip)
+	if (e.target.style.img = img){
+		console.log("correct")
+	} 
+	else{
+		transform = backCard
+		console.log("incorrect")
+	}
+
+}
+
+for (let i = 0; i < numberOfGuesses; i++;){
+	if (numberOfGuesses === 6){
+		console.log("Sorry, try again")
+	}
+
+}
